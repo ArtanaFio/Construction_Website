9712557a3b3f90120ccd569fcf06a01ecfc96566
ArtanaFio <125154911+ArtanaFio@users.noreply.github.com>
2024-01-03 12:29:18 -0500

    Add fade in effect and change fonts
    Used JavaScript setInterval() to create fade-in effect in 4 stages on
    the header and its content upon loading the web page. Specified the
    fonts used for the web page, choosing common fonts used by construction
    companies. Updated font sizes of several elements to appropriately fit
    on the web page.

--- README.md
+++ README.md
@@ -11,4 +11,16 @@ CREDITS:
 
 --- Photo by Rodolfo Quirós: https://www.pexels.com/photo/selective-focus-photography-cement-2219024/
 
+--- Montserrat font designed by Julieta Ulanovsky, Sol Matas, Juan Pablo del Peral, Jacques Le Bailly
+    Source: https://fonts.google.com/specimen/Montserrat?preview.text=We%20care%20about%20our%20clients%27%20satisfaction&query=montserrat
+
+--- Anton font designed by Vernon Adams
+    Source: https://fonts.google.com/specimen/Anton?preview.text=We%20care%20about%20our%20clients%27%20satisfaction&query=Anton
+
+--- Poppins font designed by Indian Type Foundry, Jonny Pinhorn
+    Source: https://fonts.google.com/specimen/Poppins?preview.text=We%20care%20about%20our%20clients%27%20satisfaction&query=Poppins
+
 --- Inspiration from the Level 10 Construction website: https://www.level10gc.com/
+
+WEBSITE ADDRESS:
+https://mncpartnersconstructionllc.com
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,5 @@
 
-
+/* colorchange arrow */
 
 function highlightArrow() {
     const header = document.querySelector("header");
@@ -23,3 +23,44 @@ function highlightArrow() {
     arrow.appendChild(arrowShaft);
 };
 highlightArrow();
+
+/* Fade in upon loading the web page */
+
+const header = document.querySelector("header");
+const arrow = document.getElementById("arrow");
+const navigationBar = document.querySelector("nav");
+const titles = document.getElementById("titles");
+
+let headerOpacity = 0;
+let navigationOpacity = 0;
+let titlesOpacity = 0;
+let arrowOpacity = 0;
+
+let fadeInHeader = setInterval(() => {
+    if (headerOpacity >= 1) {
+        clearInterval(fadeInHeader);
+        let fadeInTitles = setInterval(() => {
+            if (titlesOpacity >= 1) {
+                clearInterval(fadeInTitles);
+                let fadeInNavigation = setInterval(() => {
+                    if (navigationOpacity >= 1) {
+                        clearInterval(fadeInNavigation);
+                        let fadeInArrow = setInterval(() => {
+                            if (arrowOpacity >= 1) {
+                                clearInterval(fadeInArrow);
+                            }
+                            arrow.style.opacity = arrowOpacity;
+                            arrowOpacity += 0.01;
+                        }, 10);
+                    }
+                    navigationBar.style.opacity = navigationOpacity;
+                    navigationOpacity += 0.01;
+                }, 10);
+            }
+            titles.style.opacity = titlesOpacity;
+            titlesOpacity += 0.01;
+        }, 10);
+    }
+    header.style.opacity = headerOpacity;
+    headerOpacity += 0.01;
+}, 5);
