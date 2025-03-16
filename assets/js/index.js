fa7c6392d3556308c25b43b616862d29d2ec4d82
ArtanaFio <125154911+ArtanaFio@users.noreply.github.com>
2025-03-15 22:28:53 -0400

    Refactor to contain code and reduce global scope

--- assets/js/index.js
+++ assets/js/index.js
@@ -32,45 +32,47 @@ function linkArrow() {
 linkArrow();
 
 /* Fade in upon loading the web page */
+function fadeIntoView() {
+    const header = document.querySelector("header");
+    const arrow = document.getElementById("arrow");
+    const navigationBar = document.querySelector("nav");
+    const titles = document.getElementById("titles");
 
-const header = document.querySelector("header");
-const arrow = document.getElementById("arrow");
-const navigationBar = document.querySelector("nav");
-const titles = document.getElementById("titles");
+    let headerOpacity = 0;
+    let navigationOpacity = 0;
+    let titlesOpacity = 0;
+    let arrowOpacity = 0;
 
-let headerOpacity = 0;
-let navigationOpacity = 0;
-let titlesOpacity = 0;
-let arrowOpacity = 0;
-
-let fadeInHeader = setInterval(() => {
-    if (headerOpacity >= 1) {
-        clearInterval(fadeInHeader);
-        let fadeInTitles = setInterval(() => {
-            if (titlesOpacity >= 1) {
-                clearInterval(fadeInTitles);
-                let fadeInNavigation = setInterval(() => {
-                    if (navigationOpacity >= 1) {
-                        clearInterval(fadeInNavigation);
-                        let fadeInArrow = setInterval(() => {
-                            if (arrowOpacity >= 1) {
-                                clearInterval(fadeInArrow);
-                            }
-                            arrow.style.opacity = arrowOpacity;
-                            arrowOpacity += 0.01;
-                        }, 10);
-                    }
-                    navigationBar.style.opacity = navigationOpacity;
-                    navigationOpacity += 0.01;
-                }, 10);
-            }
-            titles.style.opacity = titlesOpacity;
-            titlesOpacity += 0.01;
-        }, 10);
-    }
-    header.style.opacity = headerOpacity;
-    headerOpacity += 0.01;
-}, 5);
+    let fadeInHeader = setInterval(() => {
+        if (headerOpacity >= 1) {
+            clearInterval(fadeInHeader);
+            let fadeInTitles = setInterval(() => {
+                if (titlesOpacity >= 1) {
+                    clearInterval(fadeInTitles);
+                    let fadeInNavigation = setInterval(() => {
+                        if (navigationOpacity >= 1) {
+                            clearInterval(fadeInNavigation);
+                            let fadeInArrow = setInterval(() => {
+                                if (arrowOpacity >= 1) {
+                                    clearInterval(fadeInArrow);
+                                }
+                                arrow.style.opacity = arrowOpacity;
+                                arrowOpacity += 0.01;
+                            }, 10);
+                        }
+                        navigationBar.style.opacity = navigationOpacity;
+                        navigationOpacity += 0.01;
+                    }, 10);
+                }
+                titles.style.opacity = titlesOpacity;
+                titlesOpacity += 0.01;
+            }, 10);
+        }
+        header.style.opacity = headerOpacity;
+        headerOpacity += 0.01;
+    }, 5);
+};
+fadeIntoView();
 
 function linkFacebook() {
     document.getElementById("facebook").addEventListener('click', () => {
